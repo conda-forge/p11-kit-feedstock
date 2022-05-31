94fb46fd2b4156cc16e8b66922eedf94cdea51d3
Leo Fang <leo80042@gmail.com>
2022-05-31 17:09:24 -0400

    copy config files for osx-arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $CONDA_BUILD_CROSS_COMPILATION == "1" ]]; then
+    # Get an updated config.sub and config.guess
+    cp $BUILD_PREFIX/share/gnuconfig/config.* .
+fi
+
 ./configure --prefix=$PREFIX
 make
 make install
