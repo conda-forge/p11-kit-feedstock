8b56c0f712539536bf37ad907d289d4e5e6f0c40
Leo Fang <leo80042@gmail.com>
2022-06-01 10:31:13 -0400

    WIP: set executable

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ if [[ $CONDA_BUILD_CROSS_COMPILATION == "1" ]]; then
     # Get an updated config.sub and config.guess
     cp $BUILD_PREFIX/share/gnuconfig/config.* .
     curl -L -O https://raw.githubusercontent.com/p11-glue/p11-kit/0.23.21/autogen.sh
+    chmod +x autogen.sh
     ./autogen.sh
 fi
 
