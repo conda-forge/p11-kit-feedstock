d6dec2b01242cbd33d6e35fb4b3bde0224494bb9
Leo Fang <leo80042@gmail.com>
2022-06-01 12:21:43 -0400

    Clean up

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,9 +4,10 @@ set -x
 if [[ $CONDA_BUILD_CROSS_COMPILATION == "1" ]]; then
     # Get an updated config.sub and config.guess
     cp $BUILD_PREFIX/share/gnuconfig/config.* .
-    #curl -L -O https://raw.githubusercontent.com/p11-glue/p11-kit/0.23.21/autogen.sh
-    #chmod +x autogen.sh
-    #bash ./autogen.sh
+    # We need to regenerate the configs for osx-arm64, but we can't use autogen.sh because
+    # 1. it's not included in the release tarball
+    # 2. it's a sh script, which would mess up the shell env upon execution
+    # see https://github.com/p11-glue/p11-kit/blob/7ea59012c2c81473132211e29ea8ebcc1ce31d09/autogen.sh#L19
     autoreconf --force --install --verbose
 fi
 
